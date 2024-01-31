70fd4a2aa10210592dd3f0e3149a24f9cf23554a
PranishManandhar <154613137+PranishManandhar@users.noreply.github.com>
2024-01-31 10:32:01 +0545

    datasecure

---
+++ web project/script.js
@@ -0,0 +1,10 @@
+var drop = document.getElementById("dropdown");
+const func = ()=>{
+    if (drop.style.display == 'none') {
+        drop.style.display = 'block'
+    }
+    else{
+        drop.style.display = 'none'
+        
+    }
+}
